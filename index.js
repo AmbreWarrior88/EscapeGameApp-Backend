143903873ca6d9f7258e009ec6c64b90bd490eab
AmbreWarrior88 <56254199+AmbreWarrior88@users.noreply.github.com>
2019-12-12 12:49:48 +0100

    test

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ app.get("/escape-add", async (req, res) => {
 // READ
 
 app.get("/", async (req, res) => {
+  console.log("Hey");
+
   try {
     // const foundEscapeGames = await Escape.find();
     // res.status(200).json(foundEscapeGames);
